a7ea651aee5e586b270636b6c5d1df3445818171
Sergei Rozhkov <fgup@zserg.ru>
2025-03-28 13:26:08 +0500

    en

--- .shell/zlang.sh
+++ .shell/zlang.sh
@@ -2,14 +2,14 @@
 
 source /opt/config/mod/.shell/0.sh
 
-if [ grep -q "language=en" ${MOD_CONF}/mod_data/lang.cfg ]; then
+if [ grep -q "language: en" ${MOD_CONF}/mod_data/lang.cfg ]; then
     LANG="ru"
 else
     LANG="en"
 fi
 
 echo "[zmod]
-language: ${LANG}" >${MOD_CONF}/mod_data/lang.cfg
+language: '${LANG}'" >${MOD_CONF}/mod_data/lang.cfg
 
 sync
 sleep 5
