75a3e68ce39aff608893ce98a519f68848ae2e57
Sergei Rozhkov <fgup@zserg.ru>
2025-04-13 18:23:36 +0500

    France

--- .shell/zlang.sh
+++ .shell/zlang.sh
@@ -5,10 +5,11 @@ source /opt/config/mod/.shell/0.sh
 if [ "$1" == 'en' ]; then ZLANG="en"
 else if [ "$1" == 'de' ]; then ZLANG="de"
 else if [ "$1" == 'ru' ]; then ZLANG="ru"
-else ZLANG="ru"
-fi
-fi
-fi
+else if [ "$1" == 'fr' ]; then ZLANG="fr"
+else if [ "$1" == 'es' ]; then ZLANG="es"
+else if [ "$1" == 'it' ]; then ZLANG="it"
+else ZLANG="en"
+fi; fi; fi; fi; fi; fi
 
 echo "[zmod]
 language: ${ZLANG}" >${MOD_CONF}/mod_data/lang.cfg
